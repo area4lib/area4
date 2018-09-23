ee84f3cd4ccee2f137c6d3c8f7fc2310d1d862f0
RDIL <34555510+RDIL@users.noreply.github.com>
2018-09-23 17:43:43 -0400

    Update __init__.py

--- area4/__init__.py
+++ area4/__init__.py
@@ -1,1 +1,34 @@
+# ~ Area4 Package by RDIL ~ 
+
+# Package info variables:
+name = "area4" 
+author = "https://github.com/RDIL"
+author_email = "contactspaceboom@gmail.com"
+description = "Dividers in Python, the easy way! Four different types."
+license = "MIT License"
+
+# Divider variables:
+divider1 = str("------------------------")
+divider2 = str("________________________")
+divider3 = str("........................")
+divider4 = str("⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛")
+
+# Functions:  
+def div1():
+    print(divider1)
+
+def div2():
+    print(divider2)
+
+def div3():
+    print(divider3)
+
+def div4():
+    print(divider4)
+
+def area4info():
+    print("Name:", name)
+    print("Author:", author, "\n Author Email:", author_email)
+    print("Description:", description)
+    print("License:", license)
 
