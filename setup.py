e222fee3656989cdabe97b4ce37cf3cb17ab3a99
RDIL <34555510+RDIL@users.noreply.github.com>
2018-10-02 17:48:31 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,12 +11,7 @@ setuptools.setup(
     description="Dividers in Python, the easy way! Multiple different divider looks.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/RDIL/area4",
-    project_urls={
-        "Bug Tracker": "https://github.com/RDIL/area4/issues",
-        "Documentation": "https://github.com/RDIL/area4/blob/master/README.md#area4",
-        "Source Code": "https://github.com/RDIL/area4/blob/master/area4/__init__.py",
-    }
+    url="https://rdil.github.io/area4",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -33,4 +28,9 @@ setuptools.setup(
         'Topic :: Utilities',
         'Natural Language :: English',
     ],
+    project_urls={
+        "Bug Tracker": "https://github.com/RDIL/area4/issues",
+        "Documentation": "https://github.com/RDIL/area4/blob/master/README.md#area4",
+        "Source Code": "https://github.com/RDIL/area4",
+    }
 )
