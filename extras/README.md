099d95f82e5bb451c881fd1b0b0afab143013a64
Naman Goel <namangoel31@gmail.com>
2018-10-05 15:23:04 +0530

    first commit

--- extras/README.md
+++ extras/README.md
@@ -1,3 +1,5 @@
 # extras  
 
 A collection of random stuff that is here for no reason.  
+
+Random and basic
---
+++ extras/factorial.py
@@ -0,0 +1,7 @@
+print "hello, this is a program for factorial"
+b=input("enter a number to calculate the factorial of:")
+f=1
+for i in range(1,b+1):
+    f=f*i
+print "the factorial of", b , "=", f
+    
