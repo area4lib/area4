7e9fd3c8e205c9c8ac9d9f7b7908d5638e3970bf
RDIL <34555510+RDIL@users.noreply.github.com>
2018-11-22 16:18:02 -0500

    New version soon

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="area4",
-    version="1.2.2",
+    version="1.2.3",
     author="RDIL",
     author_email="contactspaceboom@gmail.com",
     description="Dividers in Python, the easy way! Many different divider looks.",
