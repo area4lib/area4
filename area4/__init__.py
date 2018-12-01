389f7859301f1aaea15c212c74eeda1e5f46f40f
RDIL <34555510+RDIL@users.noreply.github.com>
2018-12-01 12:19:42 -0500

    Update check function

--- area4/__init__.py
+++ area4/__init__.py
@@ -2109,6 +2109,12 @@ def area4info():
 
 
 def check(__name__):
+    """
+    Tells you if the module is being run directly or being imported
+    
+    :return: If __name__ is __main__ is true or false
+    :rtype: bool
+    """
     if __name__ == "__main__":
         return True
     else:
