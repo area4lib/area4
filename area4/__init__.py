7e688b85df012baf44d546a9cdbd633ff1a9edee
Reece D <me@rdil.rocks>
2018-12-19 08:25:24 -0500

    Move function

--- area4/__init__.py
+++ area4/__init__.py
@@ -2376,15 +2376,6 @@ def area4info():
     return info
 
 
-def check(__name__):
-    """
-    Tells you if the module is being run directly or being imported
-    
-    :return: If __name__ is __main__ is true or false
-    :rtype: bool
-    """
-    if __name__ == "__main__":
-        return True
-    else:
-        return False
+def check():
+    return "This function has been moved, use area4.util.check() instead."
 
