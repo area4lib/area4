9f05794989ac4436af89ee0eea75a74bb916645d
Reece Dunham <me@rdil.rocks>
2019-01-04 14:19:35 -0500

    Update __init__.py

--- area4/__init__.py
+++ area4/__init__.py
@@ -1,8 +1,27 @@
-dividers = [ ]
-with open("dividers.txt", "r") as f:
-    dividers = f.readlines()
+def initLib():
+    dividers = []
+    with open("dividers.txt", "r") as fh:
+        lines = fh.readlines()
+    dividers = "", lines
 
-"""
-Now if you want to use a divider, instead of calling
-a function that has a number in it, just use this array.
-"""
+initLib()
+
+
+def divider(number: int):
+    """
+    Gets you the divider you requested!
+    
+    :param number: the divider number (NOT 0)
+    :raises: ValueError
+    :return: requested divider
+    :rtype: str
+    
+    :Example:
+    area4.divider(97)
+    will return 🏖️🏖️🏖️🏖️🏖️🏖️🏖️🏖️🏖️🏖️🏖️🏖️
+    """
+    
+    if number == 0:
+        raise ValueError('Please use a number bigger then 0!')
+    else:
+        return dividers[number]
