bd17d18be2691c3bfdbecd489c1b9afc8eeed53b
Reece Dunham <jumbocakefdx@gmail.com>
2019-01-17 13:32:22 -0500

    port make-div (#99)
    * port make-div
    
    * Update __init__.py
    
    * Update __init__.py
    
    * Update __init__.py
    
    * Update __init__.py
    
    * Update __init__.py
    
    * Update __init__.py
    
    * Update __init__.py
    
    * remove __name__ check
    
    * Update __init__.py

--- area4/__init__.py
+++ area4/__init__.py
@@ -8,7 +8,6 @@
 
 # Imports:
 import random
-import area4.util
 
 
 # Class:
@@ -25,18 +24,21 @@ class Dividers:
 
     # Run some needed operations:
     def __init__(self):
-        if not area4.util.check(internal_name=__name__):
-            with open("dividers.txt", "r") as fh:
-                lines = fh.readlines()
-                lines[35] = random.randint(0, 999999999999)
-                self.dividers = lines
-        else:
-            raise EnvironmentError("This file can't be run directly!")
+        """
+        Inits the class
+        :param self:
+        :return: nothing
+        """
+        with open("dividers.txt", "r") as fh:
+            lines = fh.readlines()
+            lines[35] = random.randint(0, 999999999999)
+            self.dividers = lines
 
     # Function to get a divider
     def divider(self, number: int) -> str:
         """
         Gets you the divider you requested!
+        :param self:
         :param number: the divider number (NOT 0)
         :raises: ValueError
         :return: requested divider
@@ -55,6 +57,7 @@ class Dividers:
     def area4info(self):
         """
         Gets you some info about the package
+        :param self:
         :return: Package info
         :rtype: str
         """
@@ -66,3 +69,60 @@ class Dividers:
             return info
         except SyntaxError:
             return "Use Python 3.6 or above to run this function."
+
+    def make_div(self, unit: str, length: int = 24,
+                 start: str = '', end: str = '', literal_unit=False) -> str:
+        """
+        Generates and returns a custom divider
+        :param self:
+        :param unit: str containing a repeating unit
+        :param length: The maximum length (won't be exceeded) (default: 24)
+        :param start: optional starting string
+        :param end: optional ending string
+        :param literal_unit: if True will not try to break
+        unit down into smaller repeating subunits
+        :return: a custom created divider
+        :rtype: str
+
+        :Example:
+        custom_div = make_div(unit='=-', length=40, start='<', end='=>')
+        note:: The generated string will be terminated
+        at the specified length regardless
+        of whether all the input strings have been fully replicated.
+        A unit > 1 length may
+        not be able to be replicated to extend to the full length.
+        In this situation, the
+        string will be shorter than the specified length.
+        Example: unit of 10 characters and a specified length of
+        25 will contain 2 units for
+        a total length of 20 characters.
+        """
+
+        # reduce the size if possible to extend closer to full length
+        if not literal_unit:
+            unit = self._reduce_to_unit(unit)
+        repeats = (length - len(start + end)) // len(unit)
+
+        return (start + unit * repeats + end)[0:length]
+
+    def _reduce_to_unit(self, divider: str) -> str:
+        """
+        Reduces a repeating divider to the smallest repeating unit possible
+
+        :param self:
+        :param divider: the divider
+        :return: smallest repeating unit possible
+        :rtype: str
+
+        :Example:
+        'XxXxXxX' -> 'Xx'
+        """
+        for unit_size in range(1, len(divider) // 2 + 1):
+            length = len(divider)
+            unit = divider[:unit_size]
+
+            # ignores mismatches in final characters
+            d = divider[:unit_size * (length // unit_size)]
+            if unit * (length // unit_size) == d:
+                return unit
+        return divider  # return original if smaller unit not found
