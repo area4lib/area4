96559c5751ba7d0ef3229e4ddfa2884bb1afb2a5
Reece Dunham <me@rdil.rocks>
2019-01-19 10:49:38 -0500

    Add license value

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/RDIL/area4",
+    license="MIT",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python",
