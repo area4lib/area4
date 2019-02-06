62a5ac6269ee29f9a9d2c767f5f01f309cd90bd7
Reece Dunham <me@rdil.rocks>
2019-02-06 10:27:50 -0500

    start to add CI tests

---
+++ tests/start.py
@@ -0,0 +1,7 @@
+import os
+# goto main directory:
+os.system("cd $TRAVIS_WORKING_DIR")
+# build and install package:
+os.system("python3 setup.py sdist bdist_wheel")
+os.system("cd $TRAVIS_WORKING_DIR/dist")
+os.system("python3 -m pip install --upgrade *.whl")
