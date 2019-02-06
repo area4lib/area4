05551547bad5b6cd31fb3601e9ee9fa4140605d1
Reece Dunham <me@rdil.rocks>
2019-02-06 14:09:05 -0500

    Update start.py

--- tests/start.py
+++ tests/start.py
@@ -9,7 +9,7 @@ os.system("python3 setup.py sdist bdist_wheel")
 print("built package")
 os.system("{0} {1}{2}".format("cd", dir, "/dist/"))
 print("changed to dist directory")
-os.system("{0} {1}/{2}".format("python3 -m pip install --upgrade", dir, "area4-2.0.dev0.whl"))
+os.system("{0} {1}/dist/{2}".format("python3 -m pip install --upgrade", dir, "area4-2.0.dev0.whl"))
 print("installed")
 # run the tests:
 print("time to run tests")
