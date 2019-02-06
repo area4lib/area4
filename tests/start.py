2eb015a80d23715605fc947876f47112b801e7a9
Reece Dunham <me@rdil.rocks>
2019-02-06 14:15:57 -0500

    Update start.py

--- tests/start.py
+++ tests/start.py
@@ -4,9 +4,7 @@ dir = os.getenv("TRAVIS_BUILD_DIR")
 # goto main directory:
 os.system("{0} {1}".format("cd", dir))
 print("now in working directory")
-# build and install package:
-os.system("python3 setup.py sdist bdist_wheel")
-print("built package")
+# install package:
 os.system("{0} {1}{2}".format("cd", dir, "/dist/"))
 print("changed to dist directory")
 os.system("{0} {1}/dist/{2}".format("python3 -m pip install --upgrade", dir, "area4-2.0.dev0.whl"))
