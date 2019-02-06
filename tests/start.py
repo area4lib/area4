338cd9ffdcadff281f9753a69fcfd0de2d40a5ff
Reece Dunham <me@rdil.rocks>
2019-02-06 17:03:28 -0500

    Update start.py

--- tests/start.py
+++ tests/start.py
@@ -1,17 +1,13 @@
 import os
+
 # get working directory:
 dir = os.getenv("TRAVIS_BUILD_DIR")
+
 # goto main directory:
 os.system("{0} {1}".format("cd", dir))
+
 print("now in working directory")
 # install package:
+
 os.system("{0} {1}{2}".format("cd", dir, "/dist/"))
 print("changed to dist directory")
-os.system("{0} {1}/dist/{2}".format(
-    "python3 -m pip install --upgrade",
-    dir,
-    "area4-2.0.dev0.whl"
-))
-print("installed")
-# run the tests:
-print("time to run tests")
