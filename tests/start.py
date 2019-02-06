76927fc8b2086cbab18b20620fd1f37905875ec5
Reece Dunham <me@rdil.rocks>
2019-02-06 10:48:12 -0500

    Update start.py

--- tests/start.py
+++ tests/start.py
@@ -5,3 +5,6 @@ os.system("cd $TRAVIS_WORKING_DIR")
 os.system("python3 setup.py sdist bdist_wheel")
 os.system("cd $TRAVIS_WORKING_DIR/dist")
 os.system("python3 -m pip install --upgrade *.whl")
+# run the tests:
+os.system("cd $TRAVIS_WORKING_DIR/tests/")
+os.system("python3 run-tests.py")
