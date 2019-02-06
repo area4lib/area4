897db6a0f137a314151afcbe0a7dc87b906a5f33
Reece Dunham <me@rdil.rocks>
2019-02-06 13:35:30 -0500

    formatting!

--- tests/start.py
+++ tests/start.py
@@ -2,16 +2,16 @@ import os
 # get working directory:
 dir = os.getenv("TRAVIS_BUILD_DIR")
 # goto main directory:
-os.system("cd", dir)
+os.system("{0} {1}".format("cd", dir))
 print("now in working directory")
 # build and install package:
 os.system("python3 setup.py sdist bdist_wheel")
 print("built package")
-os.system("cd", "dir" + "/dist/")
+os.system("{0} {1}{2}".format("cd", dir, "/dist/"))
 print("changed to dist directory")
 os.system("python3 -m pip install --upgrade area4-2.0.dev0.whl")
 print("installed")
 # run the tests:
-os.system("cd", dir + "/tests/")
+os.system("{0} {1}{2}".format("cd", dir, "/tests/"))
 print("time to run tests")
 os.system("python3 testmodule.py")
