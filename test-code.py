b718c4325f533fcbb29080cfdf64a023ebcc2378
Reece Dunham <me@rdil.rocks>
2019-02-06 21:21:12 -0500

    add some spacing

--- test-code.py
+++ test-code.py
@@ -1,4 +1,5 @@
-# import os so we can get ci env variables
+# import OS module so we can
+# get CI build variables
 import os
 
 # try to import area4
@@ -9,7 +10,7 @@ try:
 except ImportError:
     # at this point, area4 either isn't in site-packages
     # or not on the system at all
-    raise Exception("Failed to import beta build.")
+    raise Exception("Failed to import the library.")
 
 # make sure this is being run directly and
 # not from another python module
@@ -19,24 +20,25 @@ else:
     print("[DEBUG] Module being run directly, not exiting")
 
 # get working directory:
-print("[DEBUG] Getting working directory")
+print("[DEBUG] Getting working directory\n")
 dir = os.getenv("TRAVIS_BUILD_DIR")
+print("[DEBUG] Got working directory ({0})\n".format(dir))
 # get divider file:
 dividers_file: str = "{0}/{1}".format(dir, "area4/dividers.txt")
-print("[DEBUG] Working directory is {0}".format(dir))
-print("[DEBUG] Divider file is located at {0}".format(dividers_file))
+print("[DEBUG] Divider file is located at {0}\n".format(dividers_file))
 
 rawDividers: str
 with open(dividers_file, "r") as fh:
     rawDividers = fh.readlines()
-    print("fetched raw dividers text file")
+    print("[DEBUG] Fetched raw dividers text file")
 
 # create instance we can use
-print("[DEBUG] Creating instance of area4")
+print("[DEBUG] Creating instance of the library\n")
 d = area4.Area4Instance()
-print("created instance: {0}".format(d))
+print("[DEBUG] Created instance: {0}\n\n".format(d))
 
 # test each divider
+print("[DEBUG] Starting tests...\n\n")
 for i in range(len(rawDividers)):
     if i < 1 or i == 35:
         i = i + 1
@@ -46,16 +48,18 @@ for i in range(len(rawDividers)):
         # of the function:
         if rawDividers[i] == d.divider(i):
             # it matches
-            print("[+] Divider {0} should work.".format(i))
+            print("[+] Divider {0} should work.\n".format(i))
         else:
             # it does not match
             print("[X] Divider {0} is broken!".format(i))
-            raise ValueError("Broken divider detected!")
+            raise ValueError("Broken divider detected: number {0}!"
+                             .format(i)
+            )
     except IndexError:
         # this is thrown if a number is offset
         # in the divider array
         # what we do about it is we just
         # simply ignore it
-        print("[DEBUG] Ignoring an IndexError")
+        print("\n[DEBUG] Ignoring an IndexError")
 
-print("[DEBUG] Exiting tests")
+print("\n\n[DEBUG] Exiting tests!\n")
