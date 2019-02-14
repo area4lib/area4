5820e8190d384ed4b1cc638af37796e382323809
n8programs <43304488+N8python@users.noreply.github.com>
2019-02-13 20:02:02 -0500

    Update __init__.py

--- area4/__init__.py
+++ area4/__init__.py
@@ -68,7 +68,8 @@ class Area4Instance:
 
     def logDivider(self, div, *stuff) -> str:
         """
-        Input a string or number as a divider,
+        
+        Input a string or number as a divider.
         and a series of strings to return, divided.
         :return: newly made value
         :rtype: str
