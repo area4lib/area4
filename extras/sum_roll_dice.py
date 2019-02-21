d03f0e833ae2c6947bbf47bf7d259a2f26c41851
Reece <me@rdil.rocks>
2019-02-21 13:41:08 -0500

    thing

--- extras/sum_roll_dice.py
+++ extras/sum_roll_dice.py
@@ -14,7 +14,7 @@ def roll_dice():
         roll = random.randint(1, 6)
         sums += roll
         if(input("Enter y or n to continue : ").upper()) == 'N':
-            print(sums)      # prints the sum of the roll calls
+            print(sums)  # prints the sum of the roll calls
             break
 
 
