6e35406dd79f7b7039c519e5fc6d62dbb4d73bbe
Reece Dunham <me@rdil.rocks>
2019-02-25 07:47:32 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", mode="r") as FILE_HANDLER:
     LONG_DESCRIPTION = FILE_HANDLER.read()
 
 # Package version:
-VERSION = "2.1.0"
+VERSION = "2.1.1"
 
 setuptools.setup(
     name="area4",
